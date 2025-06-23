d3f330e114cdda2a0c2ab4bebf52b17bdb3f5a9c
Leogabii <122978943+Leogabii@users.noreply.github.com>
2025-06-23 16:14:59 +0000

    firesore

--- controllers/productos.controller.js
+++ controllers/productos.controller.js
@@ -4,15 +4,30 @@ import { db } from '../config/firestore.config.js';
 const collection = db.collection('productos');
 
 export const getAllProductos = async (req, res) => {
-  const snapshot = await collection.get();
-  const productos = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-  res.json(productos);
+  try {
+    const snapshot = await db.collection('productos').get();
+    const productos = snapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    res.json(productos);
+  } catch (error) {
+    res.status(500).json({ msg: 'Error al obtener productos', error: error.message });
+  }
 };
 
 export const getProductoById = async (req, res) => {
-  const doc = await collection.doc(req.params.id).get();
-  if (!doc.exists) return res.status(404).json({ msg: 'Producto no encontrado' });
-  res.json({ id: doc.id, ...doc.data() });
+  try {
+    const doc = await db.collection('productos').doc(req.params.id).get();
+
+    if (!doc.exists) {
+      return res.status(404).json({ msg: 'Producto no encontrado' });
+    }
+
+    res.json({ id: doc.id, ...doc.data() });
+  } catch (error) {
+    res.status(500).json({ msg: 'Error al obtener el producto', error: error.message });
+  }
 };
 
 export const createProducto = async (req, res) => {
@@ -23,12 +38,22 @@ export const createProducto = async (req, res) => {
 
 export const updateProducto = async (req, res) => {
   const id = req.params.id;
-  await collection.doc(id).update(req.body);
-  res.json({ msg: 'Producto actualizado' });
+
+  try {
+    const productoRef = db.collection('productos').doc(id);
+    await productoRef.update(req.body);
+    res.json({ msg: 'Producto actualizado' });
+  } catch (error) {
+    res.status(500).json({ msg: 'Error al actualizar', error: error.message });
+  }
 };
 
 export const deleteProducto = async (req, res) => {
-  const id = req.params.id;
-  await collection.doc(id).delete();
-  res.json({ msg: 'Producto eliminado' });
+  try {
+    const id = req.params.id;
+    await db.collection('productos').doc(id).delete();
+    res.json({ msg: 'Producto eliminado' });
+  } catch (error) {
+    res.status(500).json({ msg: 'Error al eliminar producto', error: error.message });
+  }
 };
